61330a4d07cb65a53b293ea7a74f43eb63ade348
clayton Silva <clayton.iftm@gmail.com>
2019-03-24 23:12:23 -0300

    Criação da Classe ProcessaPropriedadesCriptografado

---
+++ src/ProcessaPropriedadesCriptografado.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+
+public class ProcessaPropriedadesCriptografado {
+	public byte[] processaPropriedadesCriptografado(Map<String, Object> propriedades) {
+		String conteudo = geraConteudoPropriedades(propriedades);
+		return processaCriptografado(conteudo.getBytes());
+	}
+	private String geraConteudoPropriedades(Map<String, Object> propriedades) {
+		//gera properties
+		StringBuilder propFileBuilder = new StringBuilder();
+		for (String prop : propriedades.keySet()) {
+			propFileBuilder.append(prop+"="+propriedades.get(prop)+"\n");
+		}
+		return propFileBuilder.toString();
+	}
+	private byte[] processaCriptografado(byte[] bytes) {
+		byte[] newBytes = new byte[bytes.length];
+		for (int i = 0; i < bytes.length; i++) {
+			newBytes[i] = (byte)((bytes[i]+10) % Byte.MAX_VALUE);
+		}
+		return newBytes;
+
+}
+}
