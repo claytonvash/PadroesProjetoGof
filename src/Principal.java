32b0df671a6c27d23870595dea6c2092d0eb05b7
clayton Silva <clayton.iftm@gmail.com>
2019-03-24 21:53:30 -0300

    Versão inicial Sistema Gerador de arquivo para Refatorar

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Principal {
+
+	public static void main(String[] args)throws IOException {
+		Map<String, Object> mapa = new HashMap<>();
+		mapa.put("nome", "Carlos");
+		mapa.put("idade",32);
+
+		GeradorArquivo gerador = new GeradorArquivo();
+		gerador.gerarArquivo("c:\\Gerador\\Cripto.text", mapa, "PROPRIEDADE_CRIPTOGRAFADO");
+
+		GeradorArquivo gerador1 = new GeradorArquivo();
+		gerador1.gerarArquivo("c:\\Gerador\\XML.zip", mapa, "XML_COMPACTADO");
+
+	}
+
+}
